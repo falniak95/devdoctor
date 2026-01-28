7ecaf2aecd8006a749be620c63ba6cd5417e1173
Furkan Alniak <furkan@amc.dk>
2026-01-28 10:26:48 +0100

    Initialize devdoctor

---
+++ src/main/java/com/falniak/devdoctor/App.java
@@ -0,0 +1,13 @@
+package com.falniak.devdoctor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
